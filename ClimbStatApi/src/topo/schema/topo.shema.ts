2cdc9f4c093eece3130c2e913afee56fee35bf94
Marion CANDUSSO <marioncandusso@gmail.com>
2026-04-20 02:46:37 +0200

    fix: resolved errors and replaced deprecated baseUrl with paths

--- ClimbStatApi/src/topo/schema/topo.shema.ts
+++ ClimbStatApi/src/topo/schema/topo.shema.ts
@@ -1,31 +1,30 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Mongoose } from "mongoose";
 import { Schema as MongooseSchema } from "mongoose";
 
-@Schema({timestamps: true})
-export class Topo{
+@Schema({ timestamps: true })
+export class Topo {
     @Prop({
         required: true,
         type: MongooseSchema.Types.ObjectId,
         ref: 'Boulder'
     })
-    idBoulder: string;
+    idBoulder!: string;
 
     @Prop({
         required: true,
         type: MongooseSchema.Types.ObjectId,
         ref: 'User'
     })
-    idUser: string;
+    idUser!: string;
 
-    @Prop({required: true})
-    isFlash: boolean;
+    @Prop({ required: true })
+    isFlash!: boolean;
 
-    @Prop({required: true})
-    nbAttempts: number;
+    @Prop({ required: true })
+    nbAttempts!: number;
 
-    @Prop({required: false})
-    comment: string;
+    @Prop({ required: false })
+    comment?: string;
 }
 
-export const TopoSchema = SchemaFactory.createForClass(Topo)
+export const TopoSchema = SchemaFactory.createForClass(Topo);
